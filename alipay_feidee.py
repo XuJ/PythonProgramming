58dc9fa335369cd2493dbb0aa575addec3ee1713
Xu Jiao <jiaox900@gmail.com>
2018-08-26 16:29:25 +0800

    update alipay_feidee
    1. add clean_str function to strip space
    2. add fund_debit_eliminate function
    3. rename the columns

--- alipay_feidee.py
+++ alipay_feidee.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import re
 
 input_dir = 'data/alipay'
 ouput_dir = 'data/alipay/result'
@@ -13,13 +14,32 @@ def convert_datetime(datetime_str):
     return new_datetime_str
 
 
+def clean_str(input_str):
+    input_str = input_str.strip()
+    input_str = re.sub('\s+', ' ', input_str)
+    output_str = re.sub(' +', ' ', input_str)
+    return output_str
+
+
+def fund_debit_eliminate(product_name):
+    p = re.compile('余额宝-\d\d\d\d.\d\d.\d\d-收益发放')
+    m = p.match(product_name)
+    if m:
+        return False
+    else:
+        return True
+
+
 for csv_file in os.listdir(input_dir):
     if csv_file.endswith('.csv'):
         df = pd.read_csv(os.path.join(input_dir, csv_file), encoding='gbk', header=4, skipfooter=7,
-                         usecols=[2, 7, 8, 9, 10, 11, 15], engine='python')
-        columns = []
-        for column in df.columns:
-            columns.append(column.strip())
-        df.columns = columns
-        df['交易创建时间'] = df['交易创建时间'].apply(convert_datetime)
+                         usecols=[2, 7, 8, 9, 11, 15], engine='python')
+        df.columns = ['交易时间', '交易对方', '商品名称', '金额', '交易状态', '资金状态']
+        df['交易时间'] = df['交易时间'].apply(convert_datetime)
+        df['交易对方'] = df['交易对方'].apply(clean_str)
+        df['商品名称'] = df['商品名称'].apply(clean_str)
+        df['金额'] = df['金额'].apply(float)
+        df['交易状态'] = df['交易状态'].apply(clean_str)
+        df['资金状态'] = df['资金状态'].apply(clean_str)
+        df = df[df['商品名称'].apply(fund_debit_eliminate)]
         df.to_csv(os.path.join(ouput_dir, csv_file), encoding='gbk', index=False)
